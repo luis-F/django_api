afa7d5391b012459c414d00e9d313a096fa66928
Rafael Cerri <rafaelbcerri@gmail.com>
2020-04-11 19:13:35 -0300

    Add filters sort to get and add create bulk

--- daily_historical/serializers.py
+++ daily_historical/serializers.py
@@ -3,6 +3,12 @@ from .models import DailyHistorical
 from stock.models import Stock
 
 
+class DailyHistoricalListSerializer(serializers.ListSerializer):
+    def create(self, validate_data):
+        daily_data = [DailyHistorical(**item) for item in validate_data]
+        return DailyHistorical.objects.bulk_create(daily_data)
+
+
 class DailyHistoricalSerializer(serializers.ModelSerializer):
     stockId = serializers.PrimaryKeyRelatedField(
         queryset=Stock.objects.all(),
@@ -11,6 +17,7 @@ class DailyHistoricalSerializer(serializers.ModelSerializer):
     stockSplits = serializers.FloatField(source='stock_splits')
 
     class Meta:
+        list_serializer_class = DailyHistoricalListSerializer
         model = DailyHistorical
         fields = [
             'id',
@@ -25,12 +32,14 @@ class DailyHistoricalSerializer(serializers.ModelSerializer):
             'stockSplits'
         ]
 
-#  def create(self, validate_data):
-#         if len(validate_data['historical_data']) == 0:
-#             raise serializers.ValidationError("Necessário preenchimento dos demais campos:"
-#             +" date ,open ,high, low, close, volume, dividends, stock_splits")
-#         hist = validate_data.pop('historical_data')
-#         stock = Stocks.objects.create(**validate_data)
-#         for data in hist:
-#             Daily_historical.objects.create(ticker=stock, **data)
-#         return stock
+    #  Essa parte de validação fica aqui ou no view?
+    #  Tenho que fazer um if de cada um?
+    # def create(self, validate_data):
+    #     if len(validate_data['historical_data']) == 0:
+    #         raise serializers.ValidationError("Necessário preenchimento dos demais campos:"
+    #                                           + " date ,open ,high, low, close, volume, dividends, stock_splits")
+    #     hist = validate_data.pop('historical_data')
+    #     stock = Stocks.objects.create(**validate_data)
+    #     for data in hist:
+    #         Daily_historical.objects.create(ticker=stock, **data)
+    #     return stock
--- daily_historical/views.py
+++ daily_historical/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import DailyHistorical
 from .serializers import DailyHistoricalSerializer
 
@@ -8,3 +9,31 @@ from .serializers import DailyHistoricalSerializer
 class DailyHistoricalView(viewsets.ModelViewSet):
     queryset = DailyHistorical.objects.all()
     serializer_class = DailyHistoricalSerializer
+
+    def get_queryset(self):
+        queryset = DailyHistorical.objects.all()
+
+        stock_id = self.request.query_params.get('stock_id', None)
+        order_by = self.request.query_params.get('order_by', None)
+
+        if stock_id is not None:
+            queryset = queryset.filter(stock_id=stock_id)
+        if order_by is not None:
+            queryset = queryset.order_by(order_by)
+
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        is_many = isinstance(request.data, list)
+        if not is_many:
+            return super(BookViewSet, self).create(request, *args, **kwargs)
+        else:
+            serializer = self.get_serializer(
+                validate_data=request.data,
+                many=True
+            )
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+
+            return Response(serializer.data, status=201, headers=headers)
